09898eaa18678796ddad20cfd9750982f2a47ded
DiegoZandonadi <127964924+DiegoZandonadi@users.noreply.github.com>
2023-05-19 11:48:40 -0300

    Add files via upload

---
+++ exercicio.py
@@ -0,0 +1,30 @@
+print("Marco é gay")
+
+print("E não assume")
+
+print('Ele é meu \n "amigo"')
+
+print(" 1 * 1=",1*1 ) 
+
+print(1+1)
+
+print("10² =", 10**3)
+
+
+print("10³ =", pow(10,3))
+
+nome = str("A")
+
+x = 1
+
+x = 1+1
+
+print(nome)
+
+y = True
+
+D1 = 94.6
+
+D2 = "5"
+
+print(round(D1 + int(D2)))
